dcddd9ba8f09c2b5779b26ddb345a9474e3a6b4a
Yutong Xin <mxx@Yutongs-MacBook-Pro.local>
2020-02-05 21:44:41 -0500

    added hashCode and equals of Board

--- src/TrafficJamPuzzleHeuristicOverlap.java
+++ src/TrafficJamPuzzleHeuristicOverlap.java
@@ -1,11 +1,15 @@
 public class TrafficJamPuzzleHeuristicOverlap implements TrafficJamPuzzleHeuristic {
     /*
     f(currBoard) = number of overlaps after moving all the vehicles blocking the red car
-    for a minimum number of steps from reaching the door
+    from reaching the door for a minimum number of steps
     @Override
      */
     public int computeEstCost(Board board, int doorCoord, Vehicle redCar) {
-        // there
+        // if there exists a vehicle moving vertically in the same column with the red car,
+        // the red car is never able to reach the door
+
         return 0;
     }
+
+    private int num
 }
